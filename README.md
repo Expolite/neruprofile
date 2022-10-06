23cabcbda14cbce5eee9a3457bf3ee7ba778d418
Expolite <macompas.ronnel@gmail.com>
2022-10-06 21:54:52 +0800

    name

--- README.md
+++ README.md
@@ -10,3 +10,10 @@ git comment -m "msg here"
 
 Color use:
 bg-color: #24262B (dark-color)
+main skyBlue-color: #38d1ef (light color)
+light-color: #FEFBF6
+light-color 2: #F4F4F2
+light-color 3: #E8E8E8
+
+grey-color: #BBBFCA
+dim-grey: #495464
---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+// move firstName and lastName to original place
+setTimeout(fname_marginLft, 800); // 0.8 second
+function fname_marginLft() {
+	fname.style.marginLeft = "0rem";
+}
+
+setTimeout(fname_marginRght, 1000); // 1 second
+function fname_marginRght() {
+	lname.style.marginLeft = "5rem";
+}
